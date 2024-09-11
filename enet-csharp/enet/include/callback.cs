a8feca1fad03b3907bdcfdd87986612c877466ad
Nevin <qq2201591870@163.com>
2024-09-11 13:46:53 +0800

    callbacks

--- enet-csharp/enet/include/callback.cs
+++ enet-csharp/enet/include/callback.cs
@@ -4,18 +4,15 @@
 
 namespace enet
 {
-    public static unsafe partial class ENet
+    public unsafe struct ENetCallbacks
     {
-        public struct ENetCallbacks
-        {
-            public delegate* managed<size_t, void*> malloc;
-            public delegate* managed<void*, void> free;
+        public delegate* managed<size_t, void*> malloc;
+        public delegate* managed<void*, void> free;
 
-            public ENetCallbacks(delegate*<size_t, void*> malloc, delegate*<void*, void> free)
-            {
-                this.malloc = malloc;
-                this.free = free;
-            }
+        public ENetCallbacks(delegate*<size_t, void*> malloc, delegate*<void*, void> free)
+        {
+            this.malloc = malloc;
+            this.free = free;
         }
     }
 }
