8313260bee2268454672a44a54bc5a7f9064cb27
Nevin <qq2201591870@163.com>
2024-09-11 13:46:46 +0800

    callbacks

--- enet-csharp/enet/include/callback.cs
+++ enet-csharp/enet/include/callback.cs
@@ -1,19 +1,18 @@
-﻿// ReSharper disable ALL
+﻿using size_t = nint;
+
+// ReSharper disable ALL
 
 namespace enet
 {
-    public static unsafe partial class ENet
+    public unsafe struct ENetCallbacks
     {
-        public struct ENetCallbacks
-        {
-            public delegate* managed<nint, void*> malloc;
-            public delegate* managed<void*, void> free;
+        public delegate* managed<size_t, void*> malloc;
+        public delegate* managed<void*, void> free;
 
-            public ENetCallbacks(delegate*<nint, void*> malloc, delegate*<void*, void> free)
-            {
-                this.malloc = malloc;
-                this.free = free;
-            }
+        public ENetCallbacks(delegate*<size_t, void*> malloc, delegate*<void*, void> free)
+        {
+            this.malloc = malloc;
+            this.free = free;
         }
     }
 }
