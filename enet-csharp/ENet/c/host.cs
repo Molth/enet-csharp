dcbf0a63107825322a7d5756e173d8121bcada95
Molth Nevin <qq2201591870@163.com>
2025-03-07 11:38:41 +0800

    ping

--- enet-csharp/ENet/c/host.cs
+++ enet-csharp/ENet/c/host.cs
@@ -7,7 +7,6 @@ using static enet.ENetSocketType;
 using static enet.ENetPeerState;
 using static enet.ENetProtocolCommand;
 using static enet.ENetProtocolFlag;
-using static NanoSockets.NanoSockets;
 
 // ReSharper disable ALL
 
@@ -17,8 +16,11 @@ namespace enet
     {
         public static int enet_host_ping(ENetHost* host, ENetAddress* address)
         {
-            enet_uint8* buffer = stackalloc enet_uint8[1];
-            return nanosockets_send(host->socket, address, buffer, 1) > 0 ? 0 : -1;
+            ENetBuffer buffer;
+            enet_uint8* data = stackalloc enet_uint8[1];
+            buffer.data = data;
+            buffer.dataLength = 1;
+            return enet_socket_send(host->socket, address, &buffer, 1) > 0 ? 0 : -1;
         }
 
         public static ENetHost* enet_host_create(ENetAddress* address, size_t peerCount, size_t channelLimit, enet_uint32 incomingBandwidth, enet_uint32 outgoingBandwidth)
